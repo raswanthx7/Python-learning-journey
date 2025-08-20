0868ac0c5cb9409ae556632f092802e54af6d897
raswanthx7 <k.m.raswanth2004@gmail.com>
2025-08-20 13:34:34 +0530

    feat: add practice script

---
+++ Chapter-02-python-basics/inoutput-statement.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# ---------------------------------
+# Chapter 2 practice script
+# -------------------------------
+
+# ------------------------------
+# practicing  input/output statement
+# ----------------------------
+# code:
+
+subject = input("Enter the name of the subject:")
+print("The name of the Subjectt is:" ,subject)
+
+# ------------------------------
+
